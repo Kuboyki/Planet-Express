fa38f014ad8a1a8536cca70e8b13b3dd143b41f5
Sanbl <Sandra.blaldea@alumnos.upm.es>
2023-12-20 11:37:15 +0100

    Merge remote-tracking branch 'origin/main'

--- ListaClientes.java
+++ ListaClientes.java
@@ -1,9 +1,6 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.IllegalFormatCodePointException;
+import java.io.*;
 import java.util.Scanner;
+
 
 /**
  * Description of the class
